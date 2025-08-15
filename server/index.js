6a95378341c67fa1f15b103439024d16d2c5c123
Ömer AKDOĞAN <akdoganomer21@gmail.com>
2025-08-15 17:10:40 +0300

    djpaoıoda

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
-
+ 
 // 4) MySQL pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
